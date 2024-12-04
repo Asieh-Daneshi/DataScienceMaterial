17f0b3a2d0c5a3de7e267e5d6bf9be3221517f10
Asieh Daneshi <asieh.daneshi@gmail.com>
2024-12-04 14:45:27 +0100

    Add files via upload

--- DealingWithMissingValues.py
+++ DealingWithMissingValues.py
@@ -17,3 +17,13 @@ pd.notnull(toyArrayMissing).sum()            # counts all the nonmissing values
 toyArrayMissing = toyArrayMissing[pd.notnull(toyArrayMissing)]  # removing missing elements from array
 # for dataframes we can use dropna to deal with missing values. But, there is something to notice, dropna removes all the data that one value from them is missing. 
 # I mean if we have the values for all the parameters, but just one element is missing, the whole row will be removed. That is reasonable, right?
+data = {'Roll No': [1, 2, 3, 4, 5],
+        'Physics': [90, None, 85, 70, 60],
+        'Chemistry': [80, 75, None, 90, 85],
+        'Maths': [None, 65, 78, 82, 95],
+        'Computers': [None, 80, 72, 88, None]}
+df = pd.DataFrame(data)
+df.dropna()            # removes all the rows with one or more missing elements
+df.dropna(how='any')   # removes all the rows with one or more missing elements
+df.dropna(how='all')       # removes the rows that all the elements are missing
+df.dropna(thresh=4, axis='rows')  # keeps the rows that at least 4 elementa aren't nan
